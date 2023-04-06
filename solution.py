f530d1c01ee62c398d225c1246050ea7212aa947
mckuratova <107032112+mckuratova@users.noreply.github.com>
2023-04-06 14:43:07 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,16 +5,4 @@ import numpy as np
 chat_id = 123456 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    ni = {}
-    for i in x:
-        if i in ni:
-            ni[i] += 1
-        else:
-            ni[i] = 1
-    t = 16
-    n = len(x)
-    summa = 0
-    for i in ni:
-        summa += i * ni[i]
-    res = summa / (t * n)
-    return res
+    return sum(x) / (16 * len(x))
