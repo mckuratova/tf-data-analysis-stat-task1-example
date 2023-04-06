59d89d9b3dc3ae232e83fee98518891295314785
mckuratova <107032112+mckuratova@users.noreply.github.com>
2023-04-06 20:19:03 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 906914964 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    return sum(x) / len(x)
+    return sum(x) / (16 * len(x))
